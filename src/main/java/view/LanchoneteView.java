e6ed54db583db74e0e97e4f1d63f02dd563ff8e5
Yasmin Santos <118862284+YasKarla@users.noreply.github.com>
2025-10-06 11:24:38 -0300

    Update LanchoneteView.java

--- src/main/java/view/LanchoneteView.java
+++ src/main/java/view/LanchoneteView.java
@@ -11,12 +11,12 @@ public class LanchoneteView {
     private JButton botaoCardapio;
     private JButton botaoAcompanharPedido;
     private JLabel labelEndereco;
-    private LanchoneteControllerImpl controller; // Adicione o controlador como um atributo
+    private LanchoneteControllerImpl controller; 
 
-    public LanchoneteView(LanchoneteControllerImpl controller) { // Modifique o construtor para receber o controlador
-        this.controller = controller; // Atribua o controlador recebido ao atributo
+    public LanchoneteView(LanchoneteControllerImpl controller) { 
+        this.controller = controller; 
         inicioLanchonete();
-        adicionarListeners(); // Adiciona os ActionListeners após a inicialização da interface
+        adicionarListeners(); 
     }
 
     public void inicioLanchonete() {
@@ -30,7 +30,6 @@ public class LanchoneteView {
         Color corFundo = Color.decode("#FFCC99");
         mainPanel.setBackground(corFundo);
 
-        // Caminho absoluto para a imagem (ajuste se necessário)
         ImageIcon imagemIcon = new ImageIcon("C:\\Users\\ct67ca\\Desktop\\Nova pasta\\untitled\\src\\main\\java\\imagem\\fundo_index.png");
         Image imagem = imagemIcon.getImage();
         Image novaImagem = imagem.getScaledInstance(290, 350, Image.SCALE_SMOOTH);
@@ -44,13 +43,13 @@ public class LanchoneteView {
         centerButtonPanel.setBackground(corFundo);
 
         JPanel tituloPanel = new JPanel();
-        tituloPanel.setLayout(new FlowLayout(FlowLayout.CENTER)); // Alinha o texto no centro
-        tituloPanel.setBorder(new EmptyBorder(70, 0, 10, 40)); // Adiciona espaçamento ao redor do texto
-        tituloPanel.setBackground(corFundo); // Defina a cor de fundo do painel (opcional)
+        tituloPanel.setLayout(new FlowLayout(FlowLayout.CENTER)); 
+        tituloPanel.setBorder(new EmptyBorder(70, 0, 10, 40)); 
+        tituloPanel.setBackground(corFundo); 
 
         JLabel tituloLabel = new JLabel("Burguer Market");
-        tituloLabel.setFont(new Font("Arial", Font.BOLD, 24)); // Defina a fonte, tamanho e estilo
-        tituloLabel.setForeground(Color.BLACK); // Defina a cor da fonte
+        tituloLabel.setFont(new Font("Arial", Font.BOLD, 24)); 
+        tituloLabel.setForeground(Color.BLACK); 
         tituloPanel.add(tituloLabel);
 
         Font botaoFonte = new Font("Arial", Font.BOLD, 14);
@@ -76,7 +75,7 @@ public class LanchoneteView {
 
         mainPanel.add(centerButtonPanel, BorderLayout.CENTER);
 
-        /* Descomente as linhas abaixo para incluir o texto de endereço
+        /* 
         JPanel panelText = new JPanel();
         labelEndereco = new JLabel("Rua das Delícias, 123 | Bairro: Sabores Alegres | Telefone: (55) 9876-5432");
         panelText.add(labelEndereco);
@@ -90,7 +89,6 @@ public class LanchoneteView {
 
     public void adicionarListeners() {
         botaoCardapio.addActionListener(e -> {
-            // Quando o botão "Cardápio" é pressionado, crie e exiba a interface do cardápio
             CardapioLancheView cardapio = new CardapioLancheView(controller); // Passe o controlador para o construtor
             cardapio.exibirLanche();
         });
